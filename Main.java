65843512be3f9f7ddb2d42d26cb0385ec742056c
Tejveer Sharma <tejveer.10.sharma@gmail.com>
2024-04-02 20:25:52 +0530

    Added subtractor file

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("calculator result" + calculator.add(2,3));
+        System.out.println("subtractor result" + subtractor.sub(4,2));
     }
 }
---
+++ subtractor.java
@@ -0,0 +1,5 @@
+public class subtractor {
+    public static int sub(int a, int b){
+        return Math.abs(a-b);
+    }
+}
